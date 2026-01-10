7e04a851f9690eecf7f3c32401a0a6e54c9ced0b
Your Name <your.email@example.com>
2026-01-10 11:42:21 +0100

    commit2

--- Server.cpp
+++ Server.cpp
@@ -6,7 +6,7 @@
 /*   By: jfischer <jfischer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/09 17:23:52 by jfischer          #+#    #+#             */
-/*   Updated: 2026/01/10 11:41:13 by jfischer         ###   ########.fr       */
+/*   Updated: 2026/01/10 11:42:17 by jfischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,11 @@ void Server::setport(int port)
 
 void Server::InitServerSocket()
 {
-	sockaddr_in address;
+	sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(this->port);
+	addr.sin_addr.s_addr = INADDR_ANY;
+	
 	this->server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (this->server_fd == -1)
 	{
