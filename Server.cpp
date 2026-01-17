3596b5c32c493bc8d6dd0e4143bdd5d8c9d27b26
Eilistrae <eilistrae@Eilistrae.localdomain>
2026-01-17 14:28:12 +0100

    corrected txt-minor meaning change

--- Server.cpp
+++ Server.cpp
@@ -162,7 +162,7 @@ const char *Server::InvalidRange::what()  const throw()
 
 const char *Server::PassLengh::what() const throw()
 {
-	return ( RED "Password lengh restictions: between 3 to 16 characters" R);
+	return ( RED "Password should be between 3 to 16 characters" R);
 } 
 
 const char *Server::PassAsciOnly::what() const throw()
